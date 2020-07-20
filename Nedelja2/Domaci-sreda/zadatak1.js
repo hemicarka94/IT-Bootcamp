2a7ae3aa79e0452bc5db596d293d933d7e24cf7e
hemicarka94 <pantelic.micika.milica@gmail.com>
2020-07-20 17:10:13 +0200

    Dodajem prvi deo domaceg zadatka za sredu

---
+++ Nedelja2/Domaci-sreda/zadatak1.js
@@ -0,0 +1,10 @@
+{
+     let a = 5
+     let b = 6
+     console.log("5+6=", 5+6)
+     console.log("5-6=", 5-6)
+     console.log("5*6=", 5*6)
+     console.log("5/6=", 5/6)
+     console.log("5%6=", 5%6)
+     console.log("5^6=", 5^6)
+}
---
+++ Nedelja2/Domaci-sreda/zadatak2.js
@@ -0,0 +1,11 @@
+{
+    let cenaproizvoda = 800
+    let kupacima= 500
+    let ostatak = kupacima-cenaproizvoda
+    if (kupacima>cenaproizvoda){
+        console.log("Uspesno ste kupili proizvod. Vas preostali iznos je:" + ostatak ,"dinara" )
+    }
+    else {
+        console.log("Nemate dovoljno novca.")
+    }
+}
