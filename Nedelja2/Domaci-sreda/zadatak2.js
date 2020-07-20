17dfbf6862aca4fbe991851b1c20f38c05693329
hemicarka94 <pantelic.micika.milica@gmail.com>
2020-07-20 17:17:11 +0200

    Mala izmena

--- Nedelja2/Domaci-sreda/zadatak2.js
+++ Nedelja2/Domaci-sreda/zadatak2.js
@@ -1,9 +1,9 @@
 {
-    let cenaproizvoda = 800
+    let cenaproizvoda = 300
     let kupacima= 500
     let ostatak = kupacima-cenaproizvoda
     if (kupacima>cenaproizvoda){
-        console.log("Uspesno ste kupili proizvod. Vas preostali iznos je:" + ostatak ,"dinara" )
+        console.log("Uspesno ste kupili proizvod. Vas preostali iznos je: " + ostatak ,"dinara" )
     }
     else {
         console.log("Nemate dovoljno novca.")
