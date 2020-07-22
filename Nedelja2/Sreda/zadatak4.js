b514fbe9693d85e27402387047bf7ab439afb250
hemicarka94 <pantelic.micika.milica@gmail.com>
2020-07-22 17:55:41 +0200

    Dodatak i izmena domaceg zadatka

--- Nedelja2/Sreda/zadatak4.js
+++ Nedelja2/Sreda/zadatak4.js
@@ -1,6 +1,13 @@
 {
-    for (x=1; x<=1000; x++) {
-        if(x%5==0)
-        console.log(x)
+    // 10 : 2 = x : 1000
+    //X= (10*1000) / 2
+    // x= 5000
+    let y = []
+    for (x=1; y.length < 1000; x++) {
+        if(x%5==0) {
+            y.push(x)
+        }
     }
+    console.log('Ukpno ima brojeva:' + y.length)
+    console.log('Brojevi su sledeci:'+ y)
 }
