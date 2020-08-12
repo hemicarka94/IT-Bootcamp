600a6269721c8e25374e5a8227b0e7864c0225ab
hemicarka94 <pantelic.micika.milica@gmail.com>
2020-08-12 10:01:39 +0200

    dopuna domaceg

---
+++ Nedelja5/Sreda/divovi.js
@@ -0,0 +1,44 @@
+
+
+const divJedan= document.createElement('div')
+divJedan.id= 'oPokemonima'
+document.body.appendChild(divJedan)
+
+
+const naziv1= document.createElement('p')
+naziv1.id= 'naziv1pokemon1'
+divJedan.appendChild(naziv1)
+const slika1= document.createElement( 'img')
+slika1.id= 'pokemon1'
+divJedan.appendChild(slika1)
+
+const naziv2= document.createElement('p')
+naziv2.id= 'naziv2pokemon2'
+divJedan.appendChild(naziv2)
+const slika2= document.createElement( 'img')
+slika2.id= 'pokemon2'
+divJedan.appendChild(slika2)
+
+const naziv3= document.createElement('p')
+naziv3.id= 'naziv3pokemon3'
+divJedan.appendChild(naziv3)
+const slika3= document.createElement( 'img')
+slika3.id= 'pokemon3'
+divJedan.appendChild(slika3)
+
+const naziv4= document.createElement('p')
+naziv4.id= 'naziv4pokemon4'
+divJedan.appendChild(naziv4)
+const slika4= document.createElement( 'img')
+slika4.id= 'pokemon4'
+divJedan.appendChild(slika4)
+
+export {naziv1}
+export {slika1}
+export {naziv2}
+export {slika2}
+export {naziv3}
+export {slika3}
+export {naziv4}
+export {slika4}
+
--- Nedelja5/Sreda/dugme1.js
+++ Nedelja5/Sreda/dugme1.js
@@ -1,38 +1,5 @@
 import { nizPokemoni } from './module.js'
-
-
-const divJedan= document.createElement('div')
-divJedan.id= 'oPokemonima'
-document.body.appendChild(divJedan)
-
-const naziv1= document.createElement('p')
-naziv1.id= 'naziv1pokemon1'
-divJedan.appendChild(naziv1)
-const slika1= document.createElement( 'img')
-slika1.id= 'pokemon1'
-divJedan.appendChild(slika1)
-
-const naziv2= document.createElement('p')
-naziv2.id= 'naziv2pokemon2'
-divJedan.appendChild(naziv2)
-const slika2= document.createElement( 'img')
-slika2.id= 'pokemon2'
-divJedan.appendChild(slika2)
-
-const naziv3= document.createElement('p')
-naziv3.id= 'naziv3pokemon3'
-divJedan.appendChild(naziv3)
-const slika3= document.createElement( 'img')
-slika3.id= 'pokemon3'
-divJedan.appendChild(slika3)
-
-const naziv4= document.createElement('p')
-naziv4.id= 'naziv4pokemon4'
-divJedan.appendChild(naziv4)
-const slika4= document.createElement( 'img')
-slika4.id= 'pokemon4'
-divJedan.appendChild(slika4)
-
+import {naziv1, naziv2, naziv3, naziv4, slika1, slika2, slika3, slika4} from './divovi.js'
 
 function sviPokemoni(){
     naziv1.textContent=nizPokemoni[0].ime
